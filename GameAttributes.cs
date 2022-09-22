80b1bf06f374f9e23cf75285018e030925638a0c
athiq-coder <athiqurking@gmail.com>
2022-09-22 21:33:24 +0530

    Added tictactoe

---
+++ GameAttributes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TicTacToe_2
+{
+    public enum Player
+    {
+        First = 1,
+        Second
+    }
+
+    public enum Choice
+    {
+        x = 'x',
+        o = 'o',
+        N = ' '
+    }
+
+    public enum State
+    {
+        Win = 1,
+        Lose,
+        Draw,
+        Noresult
+    }
+
+    public enum BoardState
+    {
+        Empty = 0,
+        PartiallyFilled,
+        HalfFilled,
+        Full
+    }
+}
---
+++ main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TicTacToe_2
+{
+    public class main
+    {
+        public static void Main(string[] args)
+        {
+            TicTacToe game = new TicTacToe();
+
+            game.TossUp();
+
+            game.PlayGame();
+
+            game.ReversePlay();
+        }
+    }
+}
